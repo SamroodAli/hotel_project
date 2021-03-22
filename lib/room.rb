29a86d371aefeadf98c93f29c1a07e2cf5ccc96d
samroodAli <devsamrood@gmail.com>
2021-03-22 23:27:33 +0530

    getters for capacity and occupants

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,16 @@
 class Room
   def initialize(capacity)
     @capacity =  capacity
+    @occupants = Array.new()
+  end
+  
+  def capacity
+    @capacity
   end
 
+  def occupants
+    @occupants
+  end
+  
 end
+
